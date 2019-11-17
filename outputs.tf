60ea21163b8895a08f112e7b1a1ca6024296f25f
Terje Torkelsen <tertork@gmail.com>
2019-11-17 20:52:56 +0100

    Adding description to output and public ip prefix

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,20 @@
 output "vnet_id" {
+  description = "Virtual network id."
   value = azurerm_virtual_network.vnet.id
 }
 
 output "vnet_resource_group_name" {
+  description = "Virtual network resource group name."
   value = var.resource_group_name
 }
 
 output "vnet_name" {
+  description = "Virtual network name."
   value = azurerm_virtual_network.vnet.name
 }
 
 output "subnets" {
+  description = "Map with subnets created and their id. Used for network rules etc."
   value = {
     gateway    = azurerm_subnet.gateway.id
     firewall   = azurerm_subnet.firewall.id
@@ -20,12 +24,19 @@ output "subnets" {
 }
 
 output "firewall_private_ip" {
+  description = "Private ip of firewall."
   value = azurerm_firewall.fw.ip_configuration.0.private_ip_address
 }
 
 output "private_dns" {
+  description = "Private dns settings if configured. Id and name of private dns."
   value = var.private_dns_zone == null ? null : {
     id   = azurerm_private_dns_zone.main[0].id
     name = azurerm_private_dns_zone.main[0].name
   }
+}
+
+output "public_ip_prefix" {
+  description = "Public ip prefix of firewall."
+  value = azurerm_public_ip_prefix.fw.ip_prefix
 }
